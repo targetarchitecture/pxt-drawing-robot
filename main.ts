c2be30591a8fdb5323ae5c5d6488de19f185a84e
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2024-04-26 23:59:21 +0100

    Two ToF sensors working

--- main.ts
+++ main.ts
@@ -2,17 +2,19 @@
 TCA9548A.selectChannel(0)
 Rangefinder.init()
 
-// TCA9548A.selectChannel(1)
-// Rangefinder.init()
+TCA9548A.selectChannel(1)
+Rangefinder.init()
 
 basic.forever(function () {
 
-    // TCA9548A.selectChannel(0)
-    // let x = Rangefinder.distance()
-
     TCA9548A.selectChannel(0)
+    let x = Rangefinder.distance()
+
+    TCA9548A.selectChannel(1)
+    let y = Rangefinder.distance()
+
     led.plotBarGraph(
-        Rangefinder.distance(),
+        x+y,
         1000
     )
     basic.pause(100)
