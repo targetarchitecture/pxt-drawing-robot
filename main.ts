fdb3cefd20141c462154c8b4681542863473bfba
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2024-04-27 23:03:53 +0100

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 let y = 0
 let x = 0
-//TCA9548A.selectChannel(0)
+TCA9548A.selectChannel(0)
 Rangefinder.init()
-//TCA9548A.selectChannel(1)
+TCA9548A.selectChannel(1)
 Rangefinder.init()
 basic.forever(function () {
-  //  TCA9548A.selectChannel(0)
+TCA9548A.selectChannel(0)
 x = Rangefinder.distance()
-    //TCA9548A.selectChannel(1)
+    TCA9548A.selectChannel(1)
 y = Rangefinder.distance()
     led.plotBarGraph(
     x + y,
