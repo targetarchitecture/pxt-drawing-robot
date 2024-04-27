b08274be1683e64acebfb1e78043a97a49eac49b
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2024-04-27 22:18:32 +0100

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -18,23 +18,3 @@ basic.forever(function () {
     basic.pause(100)
 })
 
-
-/**
- * Controls a TCA9548A i2c multiplexer
- */
-//% color=65 weight=80
-//% icon="\uf550"
-namespace TCA9548A {
-    let i2cAddr = 0x70
-    /**
-    * Select the device to control SD0 to SD7
-    **/
-    //% block="Select Channel"
-    //% blockId=select_channel
-    //% channel.min=0 channel.max=7 
-    export function selectChannel(channel: number): void {
-        pins.i2cWriteNumber(i2cAddr, channel + 1, NumberFormat.UInt8BE)
-        control.waitMicros(50)
-    }
-}
-
