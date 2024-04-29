0f6c85aa5731e762938eb732c524055ec65516f9
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2024-04-29 12:49:04 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,61 +1,67 @@
-
-// TCA9548A.selectChannel(0)
-// Rangefinder.init()
-// TCA9548A.selectChannel(1)
-// Rangefinder.init()
-
-// pins.i2cWriteNumber(0x70, 1<<2, NumberFormat.UInt8BE)
-// control.waitMicros(50)
-// Rangefinder.init()
-
-pins.i2cWriteNumber(0x70, 1 << 7, NumberFormat.UInt8BE)
+/**
+ * Channel 2 = 1<<2
+ * 
+ * Channel 3 = 1<<3
+ * 
+ * Channel 4 = 1<<4
+ */
+let x = 0
+pins.i2cWriteNumber(
+112,
+1 << 2,
+NumberFormat.UInt8BE,
+false
+)
+control.waitMicros(50)
+pins.i2cWriteNumber(
+112,
+1 << 3,
+NumberFormat.UInt8BE,
+false
+)
+control.waitMicros(50)
+pins.i2cWriteNumber(
+112,
+1 << 4,
+NumberFormat.UInt8BE,
+false
+)
 control.waitMicros(50)
 Rangefinder.init()
-
-// TCA9548A.selectChannel(3)
-// Rangefinder.init()
-// TCA9548A.selectChannel(4)
-// Rangefinder.init()
-// TCA9548A.selectChannel(5)
-// Rangefinder.init()
-// TCA9548A.selectChannel(6)
-// Rangefinder.init()
-// TCA9548A.selectChannel(7)
-// Rangefinder.init()
-
 basic.forever(function () {
-
-    let x = 0
-    // TCA9548A.selectChannel(0)
-    // x =x+ Rangefinder.distance()
-
-    // TCA9548A.selectChannel(1)
-    // x = x + Rangefinder.distance()
-
-    pins.i2cWriteNumber(0x70, 1 << 7, NumberFormat.UInt8BE)
-    control.waitMicros(50)
-
-    x =  Rangefinder.distance()
-
-    // TCA9548A.selectChannel(3)
-    // x = x + Rangefinder.distance()
-
-    // TCA9548A.selectChannel(4)
-    // x = x + Rangefinder.distance()
-
-    // TCA9548A.selectChannel(5)
-    // x = x + Rangefinder.distance()
-
-    // TCA9548A.selectChannel(6)
-    // x = x + Rangefinder.distance()
-
-    // TCA9548A.selectChannel(7)
-    // x = x + Rangefinder.distance()
-
-    led.plotBarGraph(
-        x,
-        500
+    serial.writeString("" + ("----\r\n"))
+    serial.writeString("" + control.millis() + "\r\n")
+    pins.i2cWriteNumber(
+    112,
+    1 << 2,
+    NumberFormat.UInt8BE,
+    false
     )
-
-    basic.pause(100)
+    control.waitMicros(50)
+    x = x + Rangefinder.distance()
+    serial.writeString("" + control.millis() + "\r\n")
+    pins.i2cWriteNumber(
+    112,
+    1 << 3,
+    NumberFormat.UInt8BE,
+    false
+    )
+    control.waitMicros(50)
+    x = x + Rangefinder.distance()
+    serial.writeString("" + control.millis() + "\r\n")
+    pins.i2cWriteNumber(
+    112,
+    1 << 4,
+    NumberFormat.UInt8BE,
+    false
+    )
+    control.waitMicros(50)
+    x = x + Rangefinder.distance()
+    serial.writeString("" + control.millis() + "\r\n")
+    led.plotBarGraph(
+    x,
+    1500
+    )
+    serial.writeString("" + control.millis() + "\r\n")
+    basic.pause(1000)
 })
