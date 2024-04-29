6584952b1264d55f661d969e68a24e4a3406ba2c
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2024-04-29 11:54:01 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,51 +1,51 @@
 
-TCA9548A.selectChannel(0)
-Rangefinder.init()
-TCA9548A.selectChannel(1)
-Rangefinder.init()
-TCA9548A.selectChannel(2)
-Rangefinder.init()
+// TCA9548A.selectChannel(0)
+// Rangefinder.init()
+// TCA9548A.selectChannel(1)
+// Rangefinder.init()
 TCA9548A.selectChannel(3)
 Rangefinder.init()
-TCA9548A.selectChannel(4)
-Rangefinder.init()
-TCA9548A.selectChannel(5)
-Rangefinder.init()
-TCA9548A.selectChannel(6)
-Rangefinder.init()
-TCA9548A.selectChannel(7)
-Rangefinder.init()
+// TCA9548A.selectChannel(3)
+// Rangefinder.init()
+// TCA9548A.selectChannel(4)
+// Rangefinder.init()
+// TCA9548A.selectChannel(5)
+// Rangefinder.init()
+// TCA9548A.selectChannel(6)
+// Rangefinder.init()
+// TCA9548A.selectChannel(7)
+// Rangefinder.init()
 
 basic.forever(function () {
 
-let x=0
-    TCA9548A.selectChannel(0)
-    x =x+ Rangefinder.distance()
+    let x = 0
+    // TCA9548A.selectChannel(0)
+    // x =x+ Rangefinder.distance()
 
-    TCA9548A.selectChannel(1)
-    x = x + Rangefinder.distance()
-
-    TCA9548A.selectChannel(2)
-    x = x + Rangefinder.distance()
+    // TCA9548A.selectChannel(1)
+    // x = x + Rangefinder.distance()
 
     TCA9548A.selectChannel(3)
-    x = x + Rangefinder.distance()
+    x =  Rangefinder.distance()
 
-    TCA9548A.selectChannel(4)
-    x = x + Rangefinder.distance()
+    // TCA9548A.selectChannel(3)
+    // x = x + Rangefinder.distance()
 
-    TCA9548A.selectChannel(5)
-    x = x + Rangefinder.distance()
+    // TCA9548A.selectChannel(4)
+    // x = x + Rangefinder.distance()
 
-    TCA9548A.selectChannel(6)
-    x = x + Rangefinder.distance()
+    // TCA9548A.selectChannel(5)
+    // x = x + Rangefinder.distance()
 
-    TCA9548A.selectChannel(7)
-    x = x + Rangefinder.distance()
+    // TCA9548A.selectChannel(6)
+    // x = x + Rangefinder.distance()
+
+    // TCA9548A.selectChannel(7)
+    // x = x + Rangefinder.distance()
 
     led.plotBarGraph(
-    x,
-    1000
+        x,
+        500
     )
 
     basic.pause(100)
