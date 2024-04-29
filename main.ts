e46f9faa609a2778365d17f667e596a597526c90
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2024-04-29 21:42:23 +0100

    https://docs.keyestudio.com/projects/KS0426/en/latest/makecode/makecode.html

--- main.ts
+++ main.ts
@@ -1,66 +1,76 @@
 let x = 0
-
-// TCA9548A.selectChannel(3)
-// Rangefinder.init()
-// TCA9548A.selectChannel(4)
-// Rangefinder.init()
-
-// basic.forever(function () {
-//     x=0
-//     TCA9548A.selectChannel(3)
-//     x = x + Rangefinder.distance()
-
-//     TCA9548A.selectChannel(4)
-//     x = x + Rangefinder.distance()
-
-//     led.plotBarGraph(
-//     x,
-//     1500
-//     )    
-//     basic.pause(100)
-// })
-
-serial.redirectToUSB()
-irRemote.connectInfrared(DigitalPin.P16)
-k_Bit.carStop();
-
-// basic.forever(function () {
-//     x = irRemote.returnIrButton()
-//     serial.writeValue("irRemote", x)
-//     basic.pause(110)
-// })
-
-k_Bit.LED_brightness(50)
-k_Bit.Led(COLOR.blue)
-
-let val = 0
-
+pins.i2cWriteNumber(
+112,
+0,
+NumberFormat.UInt8BE,
+false
+)
+control.waitMicros(50)
+pins.i2cWriteNumber(
+112,
+1 << 2,
+NumberFormat.UInt8BE,
+false
+)
+control.waitMicros(50)
+Rangefinder.init()
+pins.i2cWriteNumber(
+112,
+1 << 3,
+NumberFormat.UInt8BE,
+false
+)
+control.waitMicros(50)
+Rangefinder.init()
+pins.i2cWriteNumber(
+112,
+1 << 4,
+NumberFormat.UInt8BE,
+false
+)
+control.waitMicros(50)
+Rangefinder.init()
 basic.forever(function () {
-
-    val = irRemote.returnIrButton()
-
-    if (val == 0) {
-        k_Bit.carStop();
-        k_Bit.Led(COLOR.black)
-    }
-    else if (val == 70) {
-        k_Bit.run(DIR.RunForward, 10)
-        k_Bit.Led(COLOR.green)
-    } 
-    else if (val == 68) {
-        k_Bit.Motor(MotorObs.LeftSide, MotorDir.Back,10)
-        k_Bit.Motor(MotorObs.RightSide, MotorDir.Forward, 10)
-        k_Bit.Led(COLOR.blue)
-    } 
-    else if (val == 67) {
-        k_Bit.Motor(MotorObs.LeftSide, MotorDir.Forward, 10)
-        k_Bit.Motor(MotorObs.RightSide, MotorDir.Back, 10)
-        k_Bit.Led(COLOR.white)
-    } 
-    else if (val == 21) {
-        k_Bit.run(DIR.RunBack, 10)
-        k_Bit.Led(COLOR.red)
-    }
-
-    basic.pause(110)
+    serial.writeString("" + ("----\r\n"))
+    pins.i2cWriteNumber(
+    112,
+    0,
+    NumberFormat.UInt8BE,
+    false
+    )
+    control.waitMicros(50)
+    serial.writeString("start of loop" + control.millis() + "\r\n")
+    pins.i2cWriteNumber(
+    112,
+    1 << 2,
+    NumberFormat.UInt8BE,
+    false
+    )
+    control.waitMicros(50)
+    x = x + Rangefinder.distance()
+    serial.writeString("ToF 2:" + control.millis() + "\r\n")
+    pins.i2cWriteNumber(
+    112,
+    1 << 3,
+    NumberFormat.UInt8BE,
+    false
+    )
+    control.waitMicros(50)
+    x = x + Rangefinder.distance()
+    serial.writeString("ToF 3:" + control.millis() + "\r\n")
+    pins.i2cWriteNumber(
+    112,
+    1 << 4,
+    NumberFormat.UInt8BE,
+    false
+    )
+    control.waitMicros(50)
+    x = x + Rangefinder.distance()
+    serial.writeString("ToF 4:" + control.millis() + "\r\n")
+    led.plotBarGraph(
+    x,
+    1500
+    )
+    serial.writeString("end of plotBarGraph:" + control.millis() + "\r\n")
+    basic.pause(1000)
 })
