e8fe58241fef5285bddf47b8bc469323faf1b931
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2024-04-29 11:26:05 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,38 @@
 let c5 = 0
-let c6 = 0
-let c7 = 0
-TCA9548A.selectChannel(5)
-Rangefinder.init()
-TCA9548A.selectChannel(6)
-Rangefinder.init()
-TCA9548A.selectChannel(7)
-Rangefinder.init()
 
+for (let i = 1; i <= 7; i++) {
+    TCA9548A.selectChannel(i)
+    Rangefinder.init()
+}
+
+
+/**
+ * TCA9548A.selectChannel(6)
+ * 
+ * Rangefinder.init()
+ * 
+ * TCA9548A.selectChannel(7)
+ * 
+ * Rangefinder.init()
+ */
 basic.forever(function () {
-    TCA9548A.selectChannel(5)
-    c5 = Rangefinder.distance()
-    TCA9548A.selectChannel(6)
-    c6 = Rangefinder.distance()
-    TCA9548A.selectChannel(7)
-    c7 = Rangefinder.distance()
+    let c7 = 0
+    let c6 = 0
 
+ 
+    for (let i = 1; i <= 7; i++) {
+   
+
+    TCA9548A.selectChannel(i)
+c5 = Rangefinder.distance()
+    // TCA9548A.selectChannel(6)
+    // c6 = Rangefinder.distance()
+    // TCA9548A.selectChannel(7)
+    // c7 = Rangefinder.distance()
     led.plotBarGraph(
-        c5 + c6 + c7,
-        1000
+    c5 + c6 + c7,
+    1000
     )
+    }
     basic.pause(100)
 })
