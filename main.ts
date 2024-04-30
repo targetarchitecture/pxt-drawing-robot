c7f80138d852c62b52ddae53ca2cca9afe2d5bd9
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2024-04-30 16:53:15 +0100

    allow speed change

--- main.ts
+++ main.ts
@@ -7,6 +7,20 @@ k_Bit.LED_brightness(50)
 k_Bit.Led(COLOR.blue)
 
 let val = 0
+let speed = 10
+basic.showNumber(speed)
+
+input.onButtonPressed(Button.A, function() {
+    speed+=10
+    speed = Math.max(speed,100)
+    basic.showNumber(speed)
+})
+
+input.onButtonPressed(Button.B, function () {
+    speed -= 10
+    speed = Math.min(speed, 10)
+    basic.showNumber(speed)
+})
 
 basic.forever(function () {
 
@@ -17,23 +31,23 @@ basic.forever(function () {
     k_Bit.Led(COLOR.black)
 
     if (val == 70) {
-        k_Bit.Motor(MotorObs.LeftSide, MotorDir.Forward, 10)
-        k_Bit.Motor(MotorObs.RightSide, MotorDir.Forward, 10)
+        k_Bit.Motor(MotorObs.LeftSide, MotorDir.Forward, speed)
+        k_Bit.Motor(MotorObs.RightSide, MotorDir.Forward, speed)
         k_Bit.Led(COLOR.green)
     }
     else if (val == 68) {
-        k_Bit.Motor(MotorObs.LeftSide, MotorDir.Back, 10)
-        k_Bit.Motor(MotorObs.RightSide, MotorDir.Forward, 10)
+        k_Bit.Motor(MotorObs.LeftSide, MotorDir.Back, speed)
+        k_Bit.Motor(MotorObs.RightSide, MotorDir.Forward, speed)
         k_Bit.Led(COLOR.blue)
     }
     else if (val == 67) {
-        k_Bit.Motor(MotorObs.LeftSide, MotorDir.Forward, 10)
-        k_Bit.Motor(MotorObs.RightSide, MotorDir.Back, 10)
+        k_Bit.Motor(MotorObs.LeftSide, MotorDir.Forward, speed)
+        k_Bit.Motor(MotorObs.RightSide, MotorDir.Back, speed)
         k_Bit.Led(COLOR.white)
     }
     else if (val == 21) {
-        k_Bit.Motor(MotorObs.LeftSide, MotorDir.Back, 10)
-        k_Bit.Motor(MotorObs.RightSide, MotorDir.Back, 10)
+        k_Bit.Motor(MotorObs.LeftSide, MotorDir.Back, speed)
+        k_Bit.Motor(MotorObs.RightSide, MotorDir.Back, speed)
         k_Bit.Led(COLOR.red)
     }
 
