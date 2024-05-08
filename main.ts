236a6bc229139eecf956c4374171af7fb25f60f8
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2024-05-08 11:30:45 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,21 @@
 
 serial.redirectToUSB()
 irRemote.connectInfrared(DigitalPin.P16)
-k_Bit.carStop();
+led.enable(true)
 
-k_Bit.LED_brightness(50)
 k_Bit.Led(COLOR.blue)
+k_Bit.LED_brightness(100)
+
+k_Bit.Motor(MotorObs.LeftSide, MotorDir.Back, 0)
+k_Bit.Motor(MotorObs.RightSide, MotorDir.Forward, 0)
 
 let val = 0
-let speed = 10
+let speed = 20
 basic.showNumber(speed)
 
-input.onButtonPressed(Button.A, function() {
-    speed+=10
-    speed = Math.min(speed,100)
+input.onButtonPressed(Button.A, function () {
+    speed += 10
+    speed = Math.min(speed, 100)
     basic.showNumber(speed)
 })
 
@@ -22,34 +25,72 @@ input.onButtonPressed(Button.B, function () {
     basic.showNumber(speed)
 })
 
+let selfDriving = false;
+
 basic.forever(function () {
 
     val = irRemote.returnIrButton()
 
-    k_Bit.Motor(MotorObs.LeftSide, MotorDir.Back, 0)
-    k_Bit.Motor(MotorObs.RightSide, MotorDir.Forward, 0)
-    k_Bit.Led(COLOR.black)
-
     if (val == 70) {
-        k_Bit.Motor(MotorObs.LeftSide, MotorDir.Forward, speed)
-        k_Bit.Motor(MotorObs.RightSide, MotorDir.Forward, speed)
+        k_Bit.Motor(MotorObs.LeftSide, MotorDir.Back, speed)
+        k_Bit.Motor(MotorObs.RightSide, MotorDir.Back, speed)
         k_Bit.Led(COLOR.green)
     }
     else if (val == 68) {
-        k_Bit.Motor(MotorObs.LeftSide, MotorDir.Back, speed)
-        k_Bit.Motor(MotorObs.RightSide, MotorDir.Forward, speed)
+        k_Bit.Motor(MotorObs.LeftSide, MotorDir.Back, 20)
+        k_Bit.Motor(MotorObs.RightSide, MotorDir.Forward, 20)
         k_Bit.Led(COLOR.blue)
     }
     else if (val == 67) {
-        k_Bit.Motor(MotorObs.LeftSide, MotorDir.Forward, speed)
-        k_Bit.Motor(MotorObs.RightSide, MotorDir.Back, speed)
+        k_Bit.Motor(MotorObs.LeftSide, MotorDir.Forward, 20)
+        k_Bit.Motor(MotorObs.RightSide, MotorDir.Back, 20)
         k_Bit.Led(COLOR.white)
     }
     else if (val == 21) {
-        k_Bit.Motor(MotorObs.LeftSide, MotorDir.Back, speed)
-        k_Bit.Motor(MotorObs.RightSide, MotorDir.Back, speed)
+        k_Bit.Motor(MotorObs.LeftSide, MotorDir.Forward, speed)
+        k_Bit.Motor(MotorObs.RightSide, MotorDir.Forward, speed)
         k_Bit.Led(COLOR.red)
     }
+    else if (val == 66 || val ==74) {
+        selfDriving = !selfDriving
+    }
+    else {
+        k_Bit.Motor(MotorObs.LeftSide, MotorDir.Back, 0)
+        k_Bit.Motor(MotorObs.RightSide, MotorDir.Forward, 0)
+        k_Bit.Led(COLOR.black)
+    }
 
-    basic.pause(110)
+
+    if (selfDriving == true) {
+
+        if (k_Bit.obstacle(MotorObs.LeftSide) == 0 && k_Bit.obstacle(MotorObs.RightSide) == 0) {
+            k_Bit.Motor(MotorObs.LeftSide, MotorDir.Forward, speed)
+            k_Bit.Motor(MotorObs.RightSide, MotorDir.Forward, speed)
+            k_Bit.Led(COLOR.red)
+        }
+       else if (k_Bit.obstacle(MotorObs.LeftSide) == 1 && k_Bit.obstacle(MotorObs.RightSide) == 1) {
+            k_Bit.Motor(MotorObs.LeftSide, MotorDir.Back, speed)
+            k_Bit.Motor(MotorObs.RightSide, MotorDir.Back, speed)
+            k_Bit.Led(COLOR.green)
+            basic.pause(400)
+        }
+        else  if (k_Bit.obstacle(MotorObs.LeftSide) == 1 && k_Bit.obstacle(MotorObs.RightSide) == 0) {
+            k_Bit.Motor(MotorObs.LeftSide, MotorDir.Forward, speed)
+            k_Bit.Motor(MotorObs.RightSide, MotorDir.Back, speed)            
+            k_Bit.Led(COLOR.white)
+            basic.pause(400)
+        }
+        else  if (k_Bit.obstacle(MotorObs.LeftSide) == 0 && k_Bit.obstacle(MotorObs.RightSide) == 1) {
+            k_Bit.Motor(MotorObs.LeftSide, MotorDir.Back, speed)
+            k_Bit.Motor(MotorObs.RightSide, MotorDir.Forward, speed)
+            k_Bit.Led(COLOR.blue)
+            basic.pause(400)
+        } else {
+            k_Bit.Motor(MotorObs.LeftSide, MotorDir.Back, 0)
+            k_Bit.Motor(MotorObs.RightSide, MotorDir.Forward, 0)
+            k_Bit.Led(COLOR.black)
+        }    }
+
+
+    basic.pause(100)
 })
